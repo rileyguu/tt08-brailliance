0f9ad2dfb377291d6f437e9c03f9d89ac0b717dd
rileygu <120695905+rileyguu@users.noreply.github.com>
2024-09-24 13:05:50 -0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,6 +1,6 @@
 `default_nettype none
 
-module braille_converter_top (
+module tt_um_brailliance (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path (unused)
